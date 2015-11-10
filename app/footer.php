54c0185f3cfeb8e4fa5070b9485ffc2c161c233d
Nic Rodriquez <nicryan@fragileinc.com>
2015-11-10 13:41:44 -0600

    adding files

---
+++ app/footer.php
@@ -0,0 +1,12 @@
+<footer class="container">
+
+	<div class="span-7">Created by <a href="http://aaron.im">Aaron Hall</a>. <i class="fa fa-thumbs-up"></i></div>
+
+	<div class="span-5 omega"><a href="http://www.gnu.org/licenses/gpl-2.0.html">Licensed under GPL v2</a>.</div>
+
+</footer><!--/.container-->
+
+<?php include("scripts.php"); ?>
+
+</body>
+</html>
---
+++ app/index.php
@@ -0,0 +1,35 @@
+<?php  $title = 'Created by Aaron Hall'; include 'header.php'; ?>
+
+<div class="container">
+
+	<div class="span-8">span-8</div>
+	<div class="span-4 omega">span-4 with omega</div>
+
+	<div class="span-3">span-3</div>
+	<div class="span-3">span-3</div>
+	<div class="span-6 omega">span-6 with omega</div>
+
+	<div class="span-4">
+
+		<img src="http://placehold.it/500x100">
+		span-4
+
+	</div><!-- /.span-4 -->
+
+	<div class="span-4">
+
+		<img src="http://placehold.it/500x100">
+		span-4
+
+	</div><!-- /.span-4 -->
+
+	<div class="span-4 omega">
+
+		<img src="http://placehold.it/500x100">
+		span-4 omega
+
+	</div><!-- /.span-4 -->
+
+</div><!--/.container-->
+
+<?php include("footer.php"); ?>
